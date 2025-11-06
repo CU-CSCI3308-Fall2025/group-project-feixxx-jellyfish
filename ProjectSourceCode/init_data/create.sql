c3e35e98ee5e80a9c0553103314074b7084607dc
IanShepKyle <iaky7079@colorado.edu>
2025-11-06 10:51:57 -0700

    expanded DB schema

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -4,6 +4,23 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS plants(
+    plant_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    sci_name VARCHAR(100),
+    plant_type VARCHAR(50),
+    season VARCHAR (50),
+    plant_description TEXT,
+    Latitude DOUBLE PRECISION,
+    Longitude DOUBLE PRECISION,
+    img_url TEXT
+);
+
+CREATE TABLE IF NOT EXISTS users_plants(
+    username foreign key references users(username),
+    plant_id foreign key references plants(id)
+);
+
 INSERT INTO users (username, password) VALUES
 ('alice', 'alicepassword'),
 ('bob', 'bobpassword'),
