49a4336b49439f2131ecc0da3753a7e393045d41
IanShepKyle <iaky7079@colorado.edu>
2025-11-06 10:24:24 -0700

    DB file with users table

---
+++ ProjectSourceCode/init_data/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS plants(
+    
+);
