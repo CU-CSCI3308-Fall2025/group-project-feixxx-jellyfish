c427fe6559c84975246ad4a61a07e09d05d173b2
Felix Conant <felix.a.conant@gmail.com>
2025-11-29 17:03:05 -0500

    Filter out null lat and lon data for map rendering

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -604,6 +604,8 @@ app.get('/api/plants', requireAuth, async (req, res) => {
       FROM plant_logs pl
       JOIN plants p ON pl.plant_id = p.plant_id
       WHERE pl.user_id = $1
+        AND p."latitude" IS NOT NULL
+        AND p."longitude" IS NOT NULL
     `, [currentUserId]);
 
     // Fetch other users' public plant logs
@@ -618,7 +620,10 @@ app.get('/api/plants', requireAuth, async (req, res) => {
              p."longitude"
       FROM plant_logs pl
       JOIN plants p ON pl.plant_id = p.plant_id
-      WHERE pl.is_public = TRUE AND pl.user_id != $1
+      WHERE pl.is_public = TRUE 
+        AND pl.user_id != $1
+        AND p."latitude" IS NOT NULL
+        AND p."longitude" IS NOT NULL
     `, [currentUserId]);
 
     return res.json({
