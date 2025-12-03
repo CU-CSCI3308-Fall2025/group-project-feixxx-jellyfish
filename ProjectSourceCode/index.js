72fdb924d4090d2fd7d4c963d06208885a8bf4db
Mathias Teferra <Mathias.Teferra.colorado.edu>
2025-12-02 22:49:58 -0700

    created and added verdant email witt an app password

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -32,8 +32,8 @@ const dbConfig = {
 const db = pgp(dbConfig);
 const mailer = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT) || 587,
-  secure: false, // set true if you're using port 465
+  port: Number(process.env.SMTP_PORT) || 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
