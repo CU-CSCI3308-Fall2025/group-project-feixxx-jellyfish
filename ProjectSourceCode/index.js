85851aee3713f3ceb083cfbb9f7ca77c06357903
Mathias Teferra <Mathias.Teferra.colorado.edu>
2025-12-04 00:23:50 -0700

    fix in email sender

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -32,8 +32,8 @@ const dbConfig = {
 const db = pgp(dbConfig);
 const mailer = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT) || 465,
-  secure: true,
+  port: Number(process.env.SMTP_PORT) || 587,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
